9ed55682e6a08f0e6436c35bd198d1fb2551ec95
eminesimsek01 <eminesarikaya09@gmail.com>
2023-02-24 23:26:16 +0300

    update

--- src/test/java/getRequest/Get03.java
+++ src/test/java/getRequest/Get03.java
@@ -1,9 +1,12 @@
 package getRequest;
 
 import baseUrls.JsonPlaceHolderBaseUrls;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Test;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class Get03 extends JsonPlaceHolderBaseUrls {
             /*
                 Given
@@ -51,6 +54,36 @@ public class Get03 extends JsonPlaceHolderBaseUrls {
         response.prettyPrint(); // like syso
 
         // Step 4: Assertion operations
-
+        /*
+         Then
+            Status Code un "200" olduğunu Assert et
+		And
+            Content Type ın "application/json" olduğunu assert et
+		And
+		    title ın “quis ut nam facilis et officia qui” olduğunu verify et.,
+		And
+		    “completed” ın false olduğunu verify et.
+		And
+		    “userId” in 1 olduğunu verify et
+         */
+        // 1. Way
+        /*
+        response.then().assertThat().statusCode(200).contentType("application/json");
+        response.
+                then().
+                assertThat().
+                body("title", Matchers.equalTo("quis ut nam facilis et officia qui"),
+                        "completed",Matchers.equalTo(false),
+                        "userId",Matchers.equalTo(1));
+        */
+        // 2. Way
+        response.
+                then().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("title", equalTo("quis ut nam facilis et officia qui"),
+                        "completed",equalTo(false),
+                        "userId",equalTo(1));
     }
 }
