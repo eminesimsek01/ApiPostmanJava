98794b79056e6264b8931ec4f336d2bb96421a04
eminesimsek01 <eminesarikaya09@gmail.com>
2023-02-25 23:35:13 +0300

    update

---
+++ src/test/java/baseUrls/DummyRestApiBaseURL.java
@@ -0,0 +1,14 @@
+package baseUrls;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+public class DummyRestApiBaseURL {
+        //BASE URL ====> https://dummy.restapiexample.com/api/v1
+    protected RequestSpecification specification;
+    @Before
+    public void setUpBaseURL(){
+        specification = new RequestSpecBuilder().
+                setBaseUri("https://dummy.restapiexample.com/api/v1").build();
+    }
+}
