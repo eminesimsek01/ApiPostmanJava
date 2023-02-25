c102d1c2066136fe9cf2b1a88cfba790c88c8326
eminesimsek01 <eminesarikaya09@gmail.com>
2023-02-25 21:36:18 +0300

    update

---
+++ src/test/java/baseUrls/TheMovieDBBaseURL.java
@@ -0,0 +1,15 @@
+package baseUrls;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+public class TheMovieDBBaseURL {
+          // https://api.themoviedb.org/3      ===,> BASE URL
+    protected RequestSpecification specification;
+    @Before
+    public void setBaseURL(){
+        specification = new RequestSpecBuilder().
+                setBaseUri("https://api.themoviedb.org/3").
+                build();
+    }
+}
