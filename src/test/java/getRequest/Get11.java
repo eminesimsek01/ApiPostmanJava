d2b13c7f05bf9ce8471c227fb2527a98a3bd818a
eminesimsek01 <eminesarikaya09@gmail.com>
2023-02-28 23:47:54 +0300

    update

--- src/test/java/getRequest/Get11.java
+++ src/test/java/getRequest/Get11.java
@@ -80,5 +80,7 @@ public class Get11 extends ApiZipopotamBaseURL {
         assertEquals(zippoPotamPojos.getCountryAbbreviation(),actualDataMap.get("country abbreviation"));
         assertEquals(zippoPotamPojos.getZippoPotamPlaces().getPlaceName(),
                 (((Map)((List)actualDataMap.get("places")).get(0))).get("place name"));
+        assertEquals(zippoPotamPojos.getZippoPotamPlaces().getLatitude(),
+                (((Map)((List)actualDataMap.get("places")).get(0)).get("latitude")));
     }
 }
---
+++ src/test/java/getRequest/Get12.java
@@ -0,0 +1,45 @@
+package getRequest;
+
+import baseUrls.GoRest;
+import org.junit.Test;
+public class Get12 extends GoRest {
+        /*
+           Given
+               https://gorest.co.in/public/v2/users/702440
+           When
+               Kullanıcı GET Methodu ile Request Gönderir
+           Then
+                Status Code un "200" olduğunu Assert et
+            And
+                Response body nin bu şekilde olduğunu doğrular
+       {
+       "id": 702440,
+       "name": "Adheesh Chopra",
+       "email": "adheesh_chopra@schamberger-langosh.biz",
+       "gender": "male",
+       "status": "active"
+        }
+        */
+    @Test
+    public void get12(){
+        //Step 1: Set URL
+        //https://gorest.co.in/public/v2/users/702440
+        specifications.pathParams("usersPath","users", "idPath","702440");
+
+        //Step 2: Set Expected Data
+        String expectedData = "{\n" +
+                "    \"id\": 702440,\n" +
+                "    \"name\": \"Adheesh Chopra\",\n" +
+                "    \"email\": \"adheesh_chopra@schamberger-langosh.biz\",\n" +
+                "    \"gender\": \"male\",\n" +
+                "    \"status\": \"active\"\n" +
+                "}";
+        //Object Mapper
+       /*
+       Object Mapper ı kullanaiblmek için
+       maven projelerinde pom.xml
+       dependcy eklemem gerekiyor.  --->> maven repo
+        */
+        System.out.println("Expected Data:\n " + expectedData);
+    }
+}
