5958796cc7979127eea4a0cca99d8c2eb8019d32
eminesimsek01 <eminesarikaya09@gmail.com>
2023-02-28 23:40:35 +0300

    update

--- src/test/java/baseUrls/ApiZipopotamBaseURL.java
+++ src/test/java/baseUrls/ApiZipopotamBaseURL.java
@@ -1,6 +1,13 @@
 package baseUrls;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
 public class ApiZipopotamBaseURL {
-    //https://api.zippopotam.us/
-    prtoected RequestSpesification;
+         //http://api.zippopotam.us
+    protected RequestSpecification specification;
+    @Before
+    public void setUpBaseURL(){
+        specification = new RequestSpecBuilder().setBaseUri("http://api.zippopotam.us").build();
+    }
 }
