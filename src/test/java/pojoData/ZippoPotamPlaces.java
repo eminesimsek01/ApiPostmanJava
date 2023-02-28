607e494a3bde2ea0231f4c57c76bd8bbd3173742
eminesimsek01 <eminesarikaya09@gmail.com>
2023-02-28 23:34:51 +0300

    update

--- src/test/java/pojoData/ZippoPotamPlaces.java
+++ src/test/java/pojoData/ZippoPotamPlaces.java
@@ -1,43 +1,80 @@
 package pojoData;
 
 public class ZippoPotamPlaces {
-      /*
-        {
-            "place name": "Maltepe Mah.",
-            "longitude": "32.3609",
-            "state": "İstanbul",
-            "state abbreviation": "34",
-            "latitude": "40.1589"
-        }
-     */
+        /*
+      "places": [
+          {
+              "place name": "Maltepe Mah.",
+              "longitude": "32.3609",
+              "state": "İstanbul",
+              "state abbreviation": "34",
+              "latitude": "40.1589"
+          }
+      ]
+  }
+           */
+        /*
+        POJO: Plain Old Java Object
+        Expected Data veya Actual Dataları depolamak için yani tutmak için kullanıyoruz.
 
-    /*
-    Pojo Class Yöntemi POJO: Plain Old Java Object
-    Expected data veya actual datayı depolamak yani tutmak için kullanıyoruz.
-    Pojo oluşturmak için takip edilmesi gerekn basamaklar;
-     */
-
-    //1.Fieldlar ----> Var PRIVATE-----> Her bir json için
-    //2.Parametresiz cons.
-    //3.Parametreli cons.
-    //4.Getter and Setter
-    //5.toString
-
+        1)Fieldlar ----> Var.     PRIVATE   -------> Her bir json için
+        2)Parametresiz Constructor
+        3)Parametreli Constructor
+        4)Gettter and Setter
+        5) toString()
+         */
     private String placeName;
-    private String longtitude;
+    private String longitude;
     private String state;
     private String stateAbbreviation;
     private String latitude;
-
     public ZippoPotamPlaces() {
     }
-
-    public ZippoPotamPlaces(String placeName, String longtitude, String state, String stateAbbreviation, String latitude) {
+    public ZippoPotamPlaces(String placeName, String longitude, String state, String stateAbbreviation, String latitude) {
         this.placeName = placeName;
-        this.longtitude = longtitude;
+        this.longitude = longitude;
         this.state = state;
         this.stateAbbreviation = stateAbbreviation;
         this.latitude = latitude;
     }
-
+    public String getPlaceName() {
+        return placeName;
+    }
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+    public String getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getStateAbbreviation() {
+        return stateAbbreviation;
+    }
+    public void setStateAbbreviation(String stateAbbreviation) {
+        this.stateAbbreviation = stateAbbreviation;
+    }
+    public String getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+    @Override
+    public String toString() {
+        return "{" +
+                "placeName='" + placeName + '\'' +
+                ", longitude='" + longitude + '\'' +
+                ", state='" + state + '\'' +
+                ", stateAbbreviation='" + stateAbbreviation + '\'' +
+                ", latitude='" + latitude + '\'' +
+                '}';
+    }
 }
