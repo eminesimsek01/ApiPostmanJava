46a1a51d51b496f1024c48311c94f8f10ac2d5c0
eminesimsek01 <eminesarikaya09@gmail.com>
2023-03-01 22:22:24 +0300

    update

--- src/test/java/getRequest/Get12.java
+++ src/test/java/getRequest/Get12.java
@@ -1,7 +1,13 @@
 package getRequest;
 
 import baseUrls.GoRest;
+import io.restassured.response.Response;
 import org.junit.Test;
+import pojoData.GoRestCoApiPojo;
+import utilities.JsonToJava;
+import java.util.HashMap;
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 public class Get12 extends GoRest {
         /*
            Given
@@ -21,10 +27,10 @@ public class Get12 extends GoRest {
         }
         */
     @Test
-    public void get12(){
+    public void get12() {
         //Step 1: Set URL
         //https://gorest.co.in/public/v2/users/702440
-        specifications.pathParams("usersPath","users", "idPath","702440");
+        specifications.pathParams("usersPath", "users", "idPath", "702440");
 
         //Step 2: Set Expected Data
         String expectedData = "{\n" +
@@ -41,5 +47,32 @@ public class Get12 extends GoRest {
        dependcy eklemem gerekiyor.  --->> maven repo
         */
         System.out.println("Expected Data:\n " + expectedData);
+
+        GoRestCoApiPojo goRestCoApiPojo = JsonToJava.convertJsonToJavaObject(expectedData, GoRestCoApiPojo.class);
+        System.out.println("goRestCoApiPojo: " + goRestCoApiPojo);
+
+        HashMap<String, Object> expectedDataMap = JsonToJava.convertJsonToJavaObject(expectedData, HashMap.class);
+        System.out.println("expectedDataMap: " + expectedDataMap);
+
+        //Step 3: Send Request
+        //pathParams("usersPath","users",
+        //               "idPath","724790");
+        Response response = given().
+                spec(specifications).
+                when().
+                get("/{usersPath}/{idPath}");
+        System.out.println("RESPONSE: ");
+        response.prettyPrint();
+
+        //Step 4: Assertion:
+
+        HashMap<String, Object> actualDataMap = JsonToJava.convertJsonToJavaObject(response.asString(), HashMap.class);
+        System.out.println("Actual Data: " + actualDataMap);
+
+        assertEquals(expectedDataMap.get("gender"), actualDataMap.get("gender"));
+        assertEquals(expectedDataMap.get("name"), actualDataMap.get("name"));
+        assertEquals(expectedDataMap.get("id"), actualDataMap.get("id"));
+        assertEquals(expectedDataMap.get("email"), actualDataMap.get("email"));
+        assertEquals(expectedDataMap.get("status"), actualDataMap.get("status"));
     }
 }
