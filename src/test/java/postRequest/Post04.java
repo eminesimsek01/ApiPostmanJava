264589e8e448fe9ed02b0e34142bf2d44716183d
eminesimsek01 <eminesarikaya09@gmail.com>
2023-03-02 23:33:10 +0300

    update

--- src/test/java/postRequest/Post04.java
+++ src/test/java/postRequest/Post04.java
@@ -6,9 +6,16 @@ import io.restassured.response.Response;
 import org.junit.Before;
 import org.junit.Test;
 import pojoData.BitPaceGetToken;
+import pojoData.CryptoCurrencyPojo;
+import pojoData.CustomerInfoPojo;
+import java.util.HashMap;
 import java.util.Map;
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
 public class Post04 {
+
+        
         /*
   
       API Doc. URL ====>>>>>   !!---https://dev.bitpace.com/bitpace-merchant-rest-api#getDepositAddressUsingPOST--!!!
@@ -21,7 +28,8 @@ public class Post04 {
       "merchant_code":"20625123425",
       "password":"a1a82a55-8355-46f1-9a95-3cd9adce495b"
   }
-
+  
+  
       Token alındıktan sonra aşğaıdaki işlemlere geçebiliriz.
           https://api-sandbox.bitpace.com/api/v1/customer/deposit/address
   
@@ -43,11 +51,18 @@ public class Post04 {
       Then
           Kullanıcı "Deposit Adress Creat" edildiğini verify eder.
           Expected message "Response Approved" içermeli
+  
        */
+        
+        // https://dev.bitpace.com/bitpace-merchant-rest-api  adresine giderek
+        //customer deposit adress gerçekleştiriniz.
+        
+        
         
     // https://api-sandbox.bitpace.com/api/v1
     String URL = "https://api-sandbox.bitpace.com/api/v1/";
     public String token;
+
     @Before
     //@Test
     public void postforToken(){
@@ -64,6 +79,7 @@ public class Post04 {
 
         // System.out.println("ReqBodyMap: " + reqBodyMap);
 
+
         //Send a Request
         Response response = given().
                 contentType(ContentType.JSON).
@@ -72,6 +88,7 @@ public class Post04 {
                 post(URLForToken);
         // System.out.println("Response: ");
         // response.prettyPrint();
+
 
         //Step 4: Assertion
         // Fakat bu case ben 4. step de RESPONSE dan token alacağım
@@ -83,7 +100,7 @@ public class Post04 {
     "code": "00",
     "message": "APPROVED",
     "status": "APPROVED"
-        }
+}
          */
         JsonPath jsonPath = response.jsonPath();
         token = jsonPath.getString("data.token");
@@ -91,8 +108,69 @@ public class Post04 {
 
 
     }
+
     @Test
     public void createDepositWithPost(){
+
+        //Set URL
+        //https://api-sandbox.bitpace.com/api/v1/customer/deposit/address
+        String URLforCreationDeposit = URL + "customer/deposit/address";
+
+
+        //Step 2: Set Req Body
+
+        /*  Kullanılacak Request Body:
+        {
+    "cryptocurrency": "BTC",
+    "customer": {
+        "reference_id": "20625123425",  //merch.code
+        "first_name":"",
+        "last_name":"",
+        "email":"drakeF@gmail.com"
+    }
+}
+    Then
+        Kullanıcı "Deposit Adress Creat" edildiğini verify eder.
+        Expected message "Response Approved" içermeli
+
+     */
+
+        CustomerInfoPojo customerInfoPojo = new CustomerInfoPojo("20625123425","","","drakeF@gmail.com");
+        CryptoCurrencyPojo reqBody = new CryptoCurrencyPojo("BTC",customerInfoPojo);
+
+
+
+        // Step 3: Send a request;
+
+        /*
+
+                body(reqBodyAndExpectedDataMap).
+                when().
+                header("Authorization","Bearer c0aa77eb8a368a7d991c8e10e6afb9756130abe80e29a6826477f8645165c7b0").
+                post("/{usersPath}");
+         */
+        Response response = given().
+                contentType(ContentType.JSON).
+                body(reqBody).
+                when().
+                header("Authorization","Bearer " + token).
+                post(URLforCreationDeposit);
+
+        System.out.println("RESPONSE: ");
+        response.prettyPrint();
+
+
+        //Step 4: Assertion
+        /*
+         Then
+        Kullanıcı "Deposit Adress Creat" edildiğini verify eder.
+        Expected message "Response Approved" içermeli
+         */
+
+        Map<String,Object> actualDataFromRESPONSE = response.as(HashMap.class);
+        System.out.println("actualDataFromRESPONSE: " + actualDataFromRESPONSE);
+
+        assertEquals("Response Approved",actualDataFromRESPONSE.get("message"));
 
     }
 
