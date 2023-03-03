31f1f11716bd1604f6422199939f7547a7935dd1
eminesimsek01 <eminesarikaya09@gmail.com>
2023-03-03 20:41:07 +0300

    update

--- src/test/java/baseUrls/RestFullBookerHerOkuAppBaseURL.java
+++ src/test/java/baseUrls/RestFullBookerHerOkuAppBaseURL.java
@@ -1,2 +1,17 @@
-package baseUrls;public class RestFullBookerHerOkuAppBaseURL {
+package baseUrls;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+public class RestFullBookerHerOkuAppBaseURL {
+
+    protected RequestSpecification specification;
+
+    @Before
+    public void setUpBaseURL(){
+
+        specification = new RequestSpecBuilder().
+                setBaseUri("https://restful-booker.herokuapp.com").
+                build();
+    }
 }
